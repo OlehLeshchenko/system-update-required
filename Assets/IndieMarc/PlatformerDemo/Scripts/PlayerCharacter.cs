5bb1628c9c97d0b068eac148a64e399ad1f3cb7f
Oleh Leshchenko <leshchenko.oleh@gmail.com>
2025-04-06 04:31:58 +0200

    Fix compile error

--- Assets/IndieMarc/PlatformerDemo/Scripts/PlayerCharacter.cs
+++ Assets/IndieMarc/PlatformerDemo/Scripts/PlayerCharacter.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
-using UnityEngine.WSA;
+// using UnityEngine.WSA;
 using UnityEngine.SceneManagement;
 
 /// <summary>
